17abd8c1bac3c4c6d98c2def4828aceaca8832e4
ionifade <idowuonifade2@gmail.com>
2026-09-22 08:31:18 +0000

    UPDATED

---
+++ trivia_game/main.py
@@ -0,0 +1,16 @@
+import random
+
+print("="*80)
+print("                     WELCOME TO THE TRIVIAL GAME!")
+print("="*80)
+
+print(random.randrange(70))
+
+print(random.randrange(0,100,5))
+
+print(random.randint(1,6))
+
+print(random.getrandbits(8))
+
+randbyte = random.randbytes(4)
+print(randbyte)
