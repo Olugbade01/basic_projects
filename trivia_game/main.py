693e933e46585e12c78cd3f3841b70d267a2944b
aakeeb <abubakarakeeb07031@gmail.com>
2026-09-25 16:33:20 +0100

    Basics

--- trivia_game/main.py
+++ trivia_game/main.py
@@ -4,13 +4,14 @@ print("="*80)
 print("                     WELCOME TO THE TRIVIAL GAME!")
 print("="*80)
 
-print(random.randrange(70))
+# print(random.randrange(70))
 
-print(random.randrange(0,100,5))
+# print(random.randrange(0,100,5))
 
-print(random.randint(1,6))
+# print(random.randint(1,6))
 
-print(random.getrandbits(8))
+# print(random.getrandbits(8))
 
-randbyte = random.randbytes(4)
-print(randbyte)
+# randbyte = random.randbytes(4)
+print(random.getrandbits(3))
+# print(randbyte)
